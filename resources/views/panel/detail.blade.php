355bee4c05218a48c058f6c7364e60bf1147ed89
Khubaib Waheed <68524430+webDevBox@users.noreply.github.com>
2021-01-11 19:47:54 +0500

    a

--- resources/views/panel/detail.blade.php
+++ resources/views/panel/detail.blade.php
@@ -27,138 +27,137 @@
 
 <div class="row text-center">
   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <button class="btn btn-success active_btn">Active Orders</button>
-    <button class="btn btn-success complete_btn mx-2">Complete Orders</button>
-    <button class="btn btn-success del_btn mx-2">Deleted Orders</button>
+    <button class="btn btn-success active_btn mx-auto mt-5">Active Orders</button>
+    <button class="btn btn-success complete_btn mx-auto mt-5">Complete Orders</button>
+    <button class="btn btn-success del_btn mx-auto mt-5">Deleted Orders</button>
   </div>
 </div>
 
 <div class="active" style="">
-<div class="row mt-3">
-    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-        <h4 class="text-success">Active Orders</h4>
+    <div class="row mt-3">
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+            <h4 class="text-success">Active Orders</h4>
+        </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
+        <input class="form-control" id="myInput" type="text" placeholder="Search..">
     </div>
-<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
-    <input class="form-control" id="myInput" type="text" placeholder="Search..">
-</div>
-</div>
-<div class="table-responsive mt-3">
-    <table class="table table-hover table-border">
+    </div>
+      <div class="table-responsive mt-3">
+          <table class="table table-hover table-border">
+              
+      <thead>
+          <tr>
+              <th> Date </th>
+              <th> Product </th>    
+              <th> Purchase Rate </th>    
+              <th> Delivery Charges </th>    
+              <th> Sale Rate </th>    
+              <th> Profit </th>    
+              <th> Action </th>    
+          </tr>  
+      </thead>
+      <tbody id="myTable">
         
-<thead>
-    <tr>
-        <th> Date </th>
-        <th> Product </th>    
-        <th> Purchase Rate </th>    
-        <th> Delivery Charges </th>    
-        <th> Sale Rate </th>    
-        <th> Profit </th>    
-        <th> Action </th>    
-    </tr>  
-</thead>
-<tbody id="myTable">
-  
-    @foreach ($active as $row)
-    <tr>
-        <td> {{$row->created_at}} </td>
-        <td> {{$row->description}} </td>    
-        <td> {{$row->purchase_rate}} </td>    
-        <td> {{$row->dc}} </td>    
-        <td> {{$row->sale_rate}} </td>    
-        <td> {{$row->profit}} </td>    
-        <td> 
-            <div class="btn-group">
-                <a href="{{route('del_order',array('id'=>$row->id))}}" title="Delete Order" class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
-                <a href="{{route('complete_order',array('id'=>$row->id))}}" title="Completed Order" class="btn btn-primary"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></a>
-            </div>
-        </td>    
-    </tr>
-    @endforeach
-</tbody>
-  </table>
-</div>
-
+          @foreach ($active as $row)
+          <tr>
+              <td> {{$row->created_at}} </td>
+              <td> {{$row->description}} </td>    
+              <td> {{$row->purchase_rate}} </td>    
+              <td> {{$row->dc}} </td>    
+              <td> {{$row->sale_rate}} </td>    
+              <td> {{$row->profit}} </td>    
+              <td> 
+                  <div class="btn-group">
+                      <a href="{{route('del_order',array('id'=>$row->id))}}" title="Delete Order" class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                      <a href="{{route('complete_order',array('id'=>$row->id))}}" title="Completed Order" class="btn btn-primary"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></a>
+                  </div>
+              </td>    
+          </tr>
+          @endforeach
+      </tbody>
+        </table>
+      </div>
 </div>
 
 
 <div class="complete" style="display:none;">
-<div class="row mt-3">
-    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-        <h4 class="text-success">Completed Orders</h4>
+    <div class="row mt-3">
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+            <h4 class="text-success">Completed Orders</h4>
+        </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
+        <input class="form-control" id="myInput1" type="text" placeholder="Search..">
     </div>
-<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
-    <input class="form-control" id="myInput1" type="text" placeholder="Search..">
-</div>
-</div>
-<div class="table-responsive mt-3">
-    <table class="table table-hover table-border">
-        
-<thead>
-    <tr>
-        <th> Date </th>
-        <th> Product </th>    
-        <th> Purchase Rate </th>    
-        <th> Delivery Charges </th>    
-        <th> Sale Rate </th>    
-        <th> Profit </th>    
-    </tr>  
-</thead>
-<tbody id="myTable1">
-  
-    @foreach ($completed as $row)
-    <tr>
-        <td> {{$row->created_at}} </td>
-        <td> {{$row->description}} </td>    
-        <td> {{$row->purchase_rate}} </td>    
-        <td> {{$row->dc}} </td>    
-        <td> {{$row->sale_rate}} </td>    
-        <td> {{$row->profit}} </td>        
-    </tr>
-    @endforeach
-</tbody>
-  </table>
-</div>
-</div>
-
-
-
-<div class="Delete" style="display:none;">
-<div class="row mt-3">
-    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-        <h4 class="text-success">Deleted Orders</h4>
     </div>
-<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
-    <input class="form-control" id="myInput2" type="text" placeholder="Search..">
-</div>
-</div>
-<div class="table-responsive mt-3">
-    <table class="table table-hover table-border">
+      <div class="table-responsive mt-3">
+          <table class="table table-hover table-border">
+              
+      <thead>
+          <tr>
+              <th> Date </th>
+              <th> Product </th>    
+              <th> Purchase Rate </th>    
+              <th> Delivery Charges </th>    
+              <th> Sale Rate </th>    
+              <th> Profit </th>    
+          </tr>  
+      </thead>
+      <tbody id="myTable1">
         
-<thead>
-    <tr>
-        <th> Date </th>
-        <th> Product </th>    
-        <th> Purchase Rate </th>    
-        <th> Delivery Charges </th>    
-        <th> Sale Rate </th>    
-        <th> Profit </th>    
-    </tr>  
-</thead>
-<tbody id="myTable2">
-  
-    @foreach ($del as $row)
-    <tr>
-        <td> {{$row->created_at}} </td>
-        <td> {{$row->description}} </td>    
-        <td> {{$row->purchase_rate}} </td>    
-        <td> {{$row->dc}} </td>    
-        <td> {{$row->sale_rate}} </td>    
-        <td> {{$row->profit}} </td>        
-    </tr>
-    @endforeach
-</tbody>
-  </table>
+          @foreach ($completed as $row)
+          <tr>
+              <td> {{$row->created_at}} </td>
+              <td> {{$row->description}} </td>    
+              <td> {{$row->purchase_rate}} </td>    
+              <td> {{$row->dc}} </td>    
+              <td> {{$row->sale_rate}} </td>    
+              <td> {{$row->profit}} </td>        
+          </tr>
+          @endforeach
+      </tbody>
+        </table>
+      </div>
 </div>
+
+
+
+<div class="delete" style="display:none;">
+    <div class="row mt-3">
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+            <h4 class="text-success">Deleted Orders</h4>
+        </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
+        <input class="form-control" id="myInput2" type="text" placeholder="Search..">
+    </div>
+    </div>
+    <div class="table-responsive mt-3">
+        <table class="table table-hover table-border">
+            
+    <thead>
+        <tr>
+            <th> Date </th>
+            <th> Product </th>    
+            <th> Purchase Rate </th>    
+            <th> Delivery Charges </th>    
+            <th> Sale Rate </th>    
+            <th> Profit </th>    
+        </tr>  
+    </thead>
+    <tbody id="myTable2">
+      
+        @foreach ($del as $row)
+        <tr>
+            <td> {{$row->created_at}} </td>
+            <td> {{$row->description}} </td>    
+            <td> {{$row->purchase_rate}} </td>    
+            <td> {{$row->dc}} </td>    
+            <td> {{$row->sale_rate}} </td>    
+            <td> {{$row->profit}} </td>        
+        </tr>
+        @endforeach
+    </tbody>
+      </table>
+    </div>
 </div>
 
 
@@ -190,7 +189,7 @@
         </div>
       </div>
     </div>
-  </div>
+</div>
   <script>
     $(document).ready(function(){
       $("#myInput").on("keyup", function() {
@@ -200,7 +199,7 @@
         });
       });
     });
-    </script>
+  </script>
   
   <script>
     $(document).ready(function(){
@@ -211,7 +210,7 @@
         });
       });
     });
-    </script>
+  </script>
   
   
   <script>
@@ -223,32 +222,30 @@
         });
       });
     });
-    </script>
+  </script>
 
 <script>
   $('.active_btn').click(function()
   {
-    $('.active').attr('style','block');
-    $('.complete').attr('style','none');
-    $('.delete').attr('style','none');
+    $('.active').attr('style','display:block;');
+    $('.complete').attr('style','display:none;');
+    $('.delete').attr('style','display:none;');
   });
-  </script>
-  <script>
-  
+</script>
+<script>
   $('.complete_btn').click(function()
   {
-    $('.active').attr('style','none');
-    $('.complete').attr('style','block');
-    $('.delete').attr('style','none');
+    $('.active').attr('style','display:none;');
+    $('.complete').attr('style','display:block;');
+    $('.delete').attr('style','display:none;');
   });
-  
 </script>
-  <script>
+<script>
   $('.del_btn').click(function()
   {
-    $('.active').attr('style','none');
-    $('.complete').attr('style','none');
-    $('.delete').attr('style','block');
+    $('.active').attr('style','display:none;');
+    $('.complete').attr('style','display:none;');
+    $('.delete').attr('style','display:block;');
   });
 </script>
 
